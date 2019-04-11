eae7d2a40803ec5511c3730bfb2881cce19ed476
linwenjun <linwenjun@163.com>
2019-04-11 22:59:06 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8u201-jre-alpine3.9
+
+ADD build/libs/eureka-0.0.1-SNAPSHOT.jar /app.jar
+
+ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+SERVER_NAME=eureka-server
+
+./gradlew build
+
+
+sudo $(aws ecr get-login --no-include-email --region cn-northwest-1)
+sudo docker build -t $SERVER_NAME .
+sudo docker tag $SERVER_NAME:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$SERVER_NAME:latest
+sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$SERVER_NAME:latest
+
+kubectl delete -f k8s-deployment.yml --kubeconfig $KUBE_CONFIG
+kubectl create -f k8s-deployment.yml --kubeconfig $KUBE_CONFIG
