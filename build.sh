7e185212c166e81d38e7a694e5af3dde4c98a3a1
linwenjun <linwenjun@163.com>
2019-04-11 23:39:09 +0800

    remove blank line

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ SERVER_NAME=eureka-server
 
 ./gradlew build
 
-
 sudo $(aws ecr get-login --no-include-email --region cn-northwest-1)
 sudo docker build -t $SERVER_NAME .
 sudo docker tag $SERVER_NAME:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$SERVER_NAME:latest
